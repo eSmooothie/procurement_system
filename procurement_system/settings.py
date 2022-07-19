71cc6a4202812a0ced4454194b97101239535a58
Sherwin Sandoval <sherwin.sandoval026@gmail.com>
2022-07-19 10:23:06 +0800

    Set sessions expire at browser close

--- procurement_system/settings.py
+++ procurement_system/settings.py
@@ -135,6 +135,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+# SESSION
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
