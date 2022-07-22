2fb22dd1fb7e15946a33b5d7682d07c7e16dfe9f
Sherwin Sandoval <sherwin.sandoval026@gmail.com>
2022-07-22 10:10:17 +0800

    Add function enable edit ppmp when user select a ppmp

--- theme/static/js/ppmp/_script.js
+++ theme/static/js/ppmp/_script.js
@@ -11,7 +11,18 @@ $(document).ready(function(e){
     $(".cost_center_ppmp_list").on('click', function(e){
         e.preventDefault();
 
+        // select all child that is not the selected and remove the class bg-gray-200
+        $("#ppmp_list_tbody").children().not(this).removeClass("bg-gray-200");
+
         var cc_ppmp_id = $(this).attr("data-ppmp-id");
+
+        $(this).addClass("bg-gray-200");
+
+        $("#edit_selected_ppmp_btn").removeAttr('disabled');
+        $("#edit_selected_ppmp_btn").removeClass("bg-gray-300 hover:bg-gray-400").addClass("hover:bg-blue-400 bg-blue-300");
+        $("#edit_selected_ppmp_btn").attr("data-selected-ppmp-id",cc_ppmp_id);
+
+        console.log($("#edit_selected_ppmp_btn"));
 
         alert("The PPMP ID is: " + cc_ppmp_id);
     });
@@ -23,6 +34,14 @@ $(document).ready(function(e){
         var cc_id = getUrlParameter('cc_id');
         
         window.location.href = base_url + window.location.pathname + "?ppmp_year=" + selected_year;
+    });
+
+    $("#edit_selected_ppmp_btn").on("click", function(e){
+        e.preventDefault();
+
+        var selected_ppmp_id = $(this).attr("data-selected-ppmp-id");
+
+        alert("Edit Selected PPMP id: "+ selected_ppmp_id);
     });
 });
 
