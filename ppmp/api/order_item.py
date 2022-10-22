1c12ef10e8e7b9cbeab0db930a0974f1cfe43636
Sherwin Sandoval <sherwin.sandoval026@gmail.com>
2022-10-22 22:25:53 +0800

    Update API

--- ppmp/api/order_item.py
+++ ppmp/api/order_item.py
@@ -1,4 +1,5 @@
 import json
+import random as rand
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from rest_framework import status
@@ -9,9 +10,9 @@ INVALID_REQUEST = {'msg':"Invalid request"}
 INVALID_ACCESS = {'msg':"Invalid access"}
 
 @login_required(login_url='login_user')
-def update_ppmp_orderitems(request):
+def put_ppmp_orderitems(request):
     
-    if request.method == 'POST':
+    if request.method == 'POST' and 'data' in request.POST:
         json_data = request.POST['data']
         parse_data = json.loads(json_data)
 
@@ -22,6 +23,7 @@ def update_ppmp_orderitems(request):
         item_id_list = list()
 
         for item in item_list:
+            # check if not empty
             if not bool(item):
                 continue
 
@@ -31,6 +33,7 @@ def update_ppmp_orderitems(request):
 
             if OrderDetails.objects.filter(ppmp_id=ppmp_id, item_desc__item__category_id=cat_id, 
                 price_id=item['price_id'], item_desc_id=item_id).exists():
+                # if order item already exist, update it's new data
 
                 order_item_model = OrderDetails.objects.get(ppmp_id=ppmp_id, item_desc__item__category_id=cat_id, 
                     price_id=item['price_id'], item_desc_id=item_id)
@@ -42,15 +45,19 @@ def update_ppmp_orderitems(request):
                 order_item_model.save()
 
             else:
+                # if new order item
                 new_order_item = OrderDetails()
                 
-                item_desc = ItemDescription.objects.get(pk=item_id)
-                ppmp = Ppmp.objects.get(pk=ppmp_id)
-                price = Prices.objects.get(pk=item['price_id'])
+                item_desc = ItemDescription.objects.get(pk=item_id) # get the item
+                ppmp = Ppmp.objects.get(pk=ppmp_id) # get the ppmp
+                price = Prices.objects.get(pk=item['price_id']) # get the item price
 
                 new_order_item.item_desc = item_desc
                 new_order_item.price = price
                 new_order_item.ppmp = ppmp
+
+                last_id = OrderDetails.objects.latest('id').id
+                new_order_item.qr_code = generate_code(last_id + 1)
 
                 new_order_item.first_quart_quant = item['first'] if float(item['first']) >= 0 else 0
                 new_order_item.second_quart_quant = item['second'] if float(item['second']) >= 0 else 0
@@ -73,11 +80,16 @@ def update_ppmp_orderitems(request):
         return JsonResponse(data, status=status.HTTP_200_OK)
     return JsonResponse(INVALID_ACCESS, status=status.HTTP_400_BAD_REQUEST)
 
+def generate_code(item_id):
+    rand_number = rand.randint(1, 9999999)
+    code = str(rand_number) + str(item_id)
+    return hash(code)
+
 def get_orderitem_logs(request):
     
-    if request.method == 'GET':
+    if request.method == 'GET' and 'orderitem_id' in request.GET:
         orderitem_id = request.GET['orderitem_id']
-        
+
         orderdetail_log = PpmpTrack.objects.select_related().filter(orderdetails_id=orderitem_id).order_by('id').all()
 
         log_records = []
@@ -121,3 +133,9 @@ def get_orderitem_logs(request):
         return JsonResponse(data, status=status.HTTP_200_OK)
 
     return JsonResponse(INVALID_REQUEST, status=status.HTTP_400_BAD_REQUEST)
+
+def get_orderitem(request):
+    pass
+
+def post_orderitem_track(request):
+    pass
--- ppmp/api/urls.py
+++ ppmp/api/urls.py
@@ -9,7 +9,7 @@ from . import order_item, app
 urlpatterns = [
     path('categories', CategoryAPI.as_view(), name="CategoryAPIsearch"),
     path('cost_center/ppmp_detail', CostCenterPPMPDetails.as_view(), name="CostCenterOrderDetailsAPIsearch"),
-    path('orderitem/update', order_item.update_ppmp_orderitems, name="OrderItem"),
+    path('orderitem/update', order_item.put_ppmp_orderitems, name="OrderItem"),
     path('orderitem/logs', order_item.get_orderitem_logs, name="OrderItemLogs"),
     path('app/get/consolidated/ppmps', app.get_consolidated_ppmp, name="get_consolidated_ppmp"),
     path('items', ItemAPI.as_view(), name="ItemAPIsearch"),
