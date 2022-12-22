e7d9affb1ce328150a1270ee9082413e85f68f2b
Replit user <>
2022-12-22 13:57:24 +0000

    replit config

--- procurement_system/settings.py
+++ procurement_system/settings.py
@@ -21,47 +21,55 @@ TAILWIND_STATIC_DIR = os.path.abspath(os.path.join(BASE_DIR, 'theme/static/'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = DJANGO_SECRET_KEY
+SECRET_KEY = "kiid!^(&$7oky%d^xh$cg#trkpcpi31wwc^3^nqs&wg0macko$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = DJANGO_DEBUG
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    "127.0.0.1",
+  "127.0.0.1", "procurementsystem.esmoothiee.repl.co", 
+  "e2fb9644-b09c-404e-aae4-42787169c787.id.repl.co", 
+  "10.20.1.103",
+  "34.149.204.188",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+  'https://*.esmoothiee.repl.co',
+  'https://*.127.0.0.1',
+]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'tailwind',
-    'theme',
-    'django_browser_reload',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'ppmp',
+  'tailwind',
+  'theme',
+  'django_browser_reload',
+  'django.contrib.admin',
+  'django.contrib.auth',
+  'django.contrib.contenttypes',
+  'django.contrib.sessions',
+  'django.contrib.messages',
+  'django.contrib.staticfiles',
+  'rest_framework',
+  'rest_framework.authtoken',
+  'ppmp',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
+  'django.middleware.security.SecurityMiddleware',
+  'django.contrib.sessions.middleware.SessionMiddleware',
+  'django.middleware.common.CommonMiddleware',
+  'django.middleware.csrf.CsrfViewMiddleware',
+  'django.contrib.auth.middleware.AuthenticationMiddleware',
+  'django.contrib.messages.middleware.MessageMiddleware',
+  'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 # star tailwindCSS
 INTERNAL_IPS = [
-    "127.0.0.1",
+  "127.0.0.1",
+  "34.149.204.188",
 ]
 
 TAILWIND_APP_NAME = 'theme'
@@ -69,69 +77,65 @@ TAILWIND_APP_NAME = 'theme'
 NPM_BIN_PATH = r'C:\Program Files\nodejs\npm.cmd'
 # end tailwindCSS
 
-
 ROOT_URLCONF = 'procurement_system.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
+  {
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [],
+    'APP_DIRS': True,
+    'OPTIONS': {
+      'context_processors': [
+        'django.template.context_processors.debug',
+        'django.template.context_processors.request',
+        'django.contrib.auth.context_processors.auth',
+        'django.contrib.messages.context_processors.messages',
+      ],
     },
+  },
 ]
 
 WSGI_APPLICATION = 'procurement_system.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': DB_ENGINE,
-        'NAME': DB_NAME,
-        'USER': DB_USER,
-        'PASSWORD': DB_PASS,
-        'HOST': DB_HOST,
-        'PORT': DB_PORT,
-    }
+  'default': {
+    'ENGINE': "django.db.backends.postgresql",
+    'NAME': "chrvotqz",
+    'USER': "chrvotqz",
+    'PASSWORD': "o8VRUw3vOpBkG8Z_3PMUz1cnqQ1G9RQa",
+    'HOST': "tiny.db.elephantsql.com",
+    'PORT': "5432",
+  }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+  {
+    'NAME':
+    'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+  },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Manila'
+TIME_ZONE = "Asia/Manila"
 
 USE_I18N = True
 
@@ -154,10 +158,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # DJANGO REST FRAMEWORK
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ]
+  'DEFAULT_AUTHENTICATION_CLASSES': [
+    'rest_framework.authentication.BasicAuthentication',
+    'rest_framework.authentication.SessionAuthentication',
+  ]
 }
 
 # MESSAGE
@@ -165,32 +169,32 @@ REST_FRAMEWORK = {
 
 # Logging
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '[{asctime} {levelname} {module}.{funcName}] : {message}',
-            'datefmt' : '%Y-%m-%d %H:%M:%S',
-            'style': '{',
-        },
-        'simple': {
-            'format': '{levelname} {message}',
-            'style': '{',
-        },
+  'version': 1,
+  'disable_existing_loggers': False,
+  'formatters': {
+    'verbose': {
+      'format': '[{asctime} {levelname} {module}.{funcName}] : {message}',
+      'datefmt': '%Y-%m-%d %H:%M:%S',
+      'style': '{',
     },
-    'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': LOG_DIR + LOG_FILE_NAME,
-            'formatter': 'verbose'
-        },
+    'simple': {
+      'format': '{levelname} {message}',
+      'style': '{',
     },
-    'loggers': {
-        'file_log': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': True,
-        },
+  },
+  'handlers': {
+    'file': {
+      'level': 'INFO',
+      'class': 'logging.FileHandler',
+      'filename': LOG_DIR + LOG_FILE_NAME,
+      'formatter': 'verbose'
     },
+  },
+  'loggers': {
+    'file_log': {
+      'handlers': ['file'],
+      'level': 'INFO',
+      'propagate': True,
+    },
+  },
 }
